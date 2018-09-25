43c5f38ad0f38b6261ebb16b5d8da21a723c70ba
Delonge12345 <lobkov1997@gmail.com>
2018-09-25 23:17:23 +0300

    third commit

---
+++ src/static/js/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    $('.slider-content__middle').slick({
+    
+  slidesToShow: 1,
+
+  arrows:true,
+  
+  prevArrow: '<div class="slider__left-button"><div class="left-button__triangle"></div>',
+  nextArrow: '<div class="slider__right-button"><div class="right-button__triangle"></div>',
+  
+    });
+    
+  });
+
+  
