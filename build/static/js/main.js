5ad560c2e27381dfe04444f605f8c5a0d5c1d1ef
Delonge12345 <lobkov1997@gmail.com>
2018-10-14 19:41:33 +0300

    second commit

---
+++ build/static/js/main.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+  $('.slider-content__middle').slick({
+    infinite: true,
+    prevArrow: $('.slider__left-button'),
+    nextArrow: $('.slider__right-button'),
+    
+  });
+
+});
+
