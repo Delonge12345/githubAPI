6b29970ca023ea2bb0c2053cf51b2b7ab0f0e22e
Delonge12345 <lobkov1997@gmail.com>
2018-10-14 19:56:26 +0300

    ttttt

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,16 +1,11 @@
 
-$(document).ready(function(){
-    $('.slider-content__middle').slick({
-    
-  slidesToShow: 1,
-
-  arrows:true,
-  
-  prevArrow: '<div class="slider__left-button"><div class="left-button__triangle"></div>',
-  nextArrow: '<div class="slider__right-button"><div class="right-button__triangle"></div>',
-  
-    });
+$(document).ready(function () {
+  $('.slider-content__middle').slick({
+    infinite: true,
+    prevArrow: $('.slider__left-button'),
+    nextArrow: $('.slider__right-button'),
     
   });
 
-  
+});
+
